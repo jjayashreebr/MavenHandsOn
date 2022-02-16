0126f57429d774ca66ca84e8c97335a4c2fda376
jjayashreebr <jayashree.j@brillio.com>
2022-02-15 16:00:03 -0800

    maven demo

---
+++ src/test/java/com/brillio/AllTests.java
@@ -0,0 +1,17 @@
+package com.brillio;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class AllTests {
+   @Test
+   public void testAdd() {
+      String str = "TestNG is working";
+      assertEquals("TestNG is working fine", str);
+   }
+
+
+}
+
